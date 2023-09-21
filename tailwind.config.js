3d5d16aacc3d6e0429e82fa9bc4bf35e016f71d4
Sarah Ibrahim <sarahibrahimabdelhamid@gmail.com>
2023-09-21 05:48:10 +0300

    la

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,35 +10,44 @@ module.exports = {
 
 ],
   theme: {
-  colors:{
-    'greyhr': '#CFD8DC',
-    'footerhr':'#8D8D8D',
-    'footericon':'#6F858F',
-    'iconbg':'#CFD8DC',
-    'buttonbg':'#263238',
-    'footerend':'#59656A' ,
-    'carouseldiv':'#3E2723' ,
-    'indicatorprev':'#D7CCC8',
-    'navopacity':'#92989B'
-  },
-  fontFamily:{
-    'Roboto':'Roboto,Roboto Condensed,sans-serif'
-  },
-  fontSize:{
-    'hr5':'5px'
-  },
-  extend:
-  {spacing: {
-    '111':'calc(100% + 64px)',
-    '1': '8px',
-    '2': '12px',
-    '3': '16px',
-    '4': '24px',
-    '5': '32px',
-    '6': '48px',
+    extend:{
+      spacing: {
+        '111':'calc(100% + 64px)',
+        '1': '8px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px'
+      }
+    }
+   ,
+   colors:{
+      'greyhr': '#CFD8DC',
+      'footerhr':'#8D8D8D',
+      'footericon':'#6F858F',
+      'iconbg':'#CFD8DC',
+      'buttonbg':'#263238',
+      'footerend':'#59656A' ,
+      'carouseldiv':'#3E2723' ,
+      'indicatorprev':'#D7CCC8',
+      'navopacity':'#92989B',
+      'white':'#FFFFFF'
 
-  }
-}
+   }
+  ,
+  extend:{
+    fontFamily:{
+      'Roboto':'Roboto,Roboto Condensed,sans-serif'
+    }
+  },
+  extend:{
+    fontSize:{
+      'hr5':'5px'
+    }
+  },
+
+
 
 
   },
